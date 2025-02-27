7a75bfd6f6b0a7887ca58c749b8e59c5a3aa161a
Niketha-S <nikisen07@gmail.com>
2025-02-27 12:55:29 -0600

    Initial commit

---
+++ src/lib/index.ts
@@ -0,0 +1,14 @@
+// place files you want to import through the `$lib` alias in this folder.
+import Bar from './Bar.svelte'
+export {Bar}
+
+import Q1 from './Q1.svelte'
+export {Q1}
+
+import Q2 from './Q2.svelte'
+export {Q2}
+
+import Scatter from './Scatter.svelte'
+import Line from './Line.svelte'
+
+export {Scatter, Line}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Assignment Sample</h1> 
+
+
+<a href="https://niketha-s.github.io/niki-vis-5609-main-2/" target="_blank" rel="noopener noreferrer">
+  A0<br>
+</a>
+
+<a href="https://danhuazhang.github.io/assignment/A1" target="_blank" rel="noopener noreferrer">
+  A1
+</a>
+
+
+<a href="https://niketha-s.github.io/niki-vis-5609-main-2/" target="_blank" rel="noopener noreferrer">
+  A2<br>
+</a>
